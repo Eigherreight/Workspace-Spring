89896668c987a5d64f55c5eab97172c4c0470e15
Eigherreight <161152721+Eigherreight@users.noreply.github.com>
2025-04-04 17:43:57 +0200

    Initial commit

---
+++ demo/src/main/java/characters/Character.java
@@ -0,0 +1,14 @@
+package characters;
+
+public class Character {
+	
+	public Character(String classe1, String stirpe, int livello1)
+	{
+		
+	}
+	
+	public Character(String classe1, String classe2, String stirpe, int livello1, int livello2)
+	{
+		
+	}
+}
